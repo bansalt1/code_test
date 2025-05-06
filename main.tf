2161ab0a4fe1ff693c880af96bbf37bfc7945b8a
bansalt1 <125238093+bansalt1@users.noreply.github.com>
2025-05-06 13:09:36 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,8 +15,13 @@ resource "null_resource" "null3" { }
 resource "null_resource" "null4" { }
 
 resource "random_string" "random" {
-  length           = 15
+  length           = 10
   special          = true
   override_special = "/@£$"
 }
+resource "time_sleep" "wait_30_seconds" {
+  depends_on = [random_string.random]
+  create_duration = "300s"
+}
+
 ####
