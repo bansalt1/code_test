800eab82bf315b47c31f67bf1798bce985ffe290
bansalt1 <125238093+bansalt1@users.noreply.github.com>
2025-06-18 15:16:29 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,8 +11,8 @@ terraform {
   }
 }
 
-resource "null_resource" "null11" { }
-resource "null_resource" "null10" { }
+resource "null_resource" "null1" { }
+resource "null_resource" "null2" { }
 
 resource "random_string" "random" {
   length           = 11
